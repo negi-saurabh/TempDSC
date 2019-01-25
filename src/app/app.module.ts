882ca9ecbc4e104ad0a746999aed50e7bbe4d004
AAAAAHui <gigglehui@outlook.com>
2019-01-25 14:05:55 +0100

    1.0 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-
+import { SiteComponent } from './site/site.component';
+import { ReviewComponent } from './review/review.component';
+import { LooComponent } from './loo/loo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	SiteComponent,
+	LooComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-site.ts
@@ -0,0 +1,7 @@
+import { Site } from './site';
+
+export const SITES: Site[] = [
+  { siteId: 11, type: 'School', nameOfBuilding: 'VU Amsterdam', floor:3, openHours: '9am - 5pm' },
+  { siteId: 2, type: 'Public', nameOfBuilding: 'aa', floor:0, openHours: 'aa' },
+  { siteId: 12, type: 'Market', nameOfBuilding: 'Jumbo', floor: -1, openHours: '2:00 - 23:00' }
+];
